cd9947c2e4066d90a34b7cb5304d76669d07450d
Yan <phanlannhi777@gmail.com>
2024-04-25 17:19:25 +0700

    update Cafe

---
+++ ProjectApp/app/src/main/java/app/Cafe.java
@@ -0,0 +1,107 @@
+package space.app;
+
+public class Cafe {
+    private String resName;
+    private String address;
+    private String describe;
+    private Float price;
+    private String menu;
+    private String timeOpen;
+    private String contact;
+    private String images;
+    private String evaluate;
+    private String link;
+
+    public Cafe(String resName, String address, String describe, Float price, String menu, String timeOpen, String contact, String images, String evaluate, String link) {
+        this.resName = resName;
+        this.address = address;
+        this.describe = describe;
+        this.price = price;
+        this.menu = menu;
+        this.timeOpen = timeOpen;
+        this.contact = contact;
+        this.images = images;
+        this.evaluate = evaluate;
+        this.link = link;
+    }
+
+    public String getResName() {
+        return resName;
+    }
+
+    public void setResName(String resName) {
+        this.resName = resName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDescribe() {
+        return describe;
+    }
+
+    public void setDescribe(String describe) {
+        this.describe = describe;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public String getMenu() {
+        return menu;
+    }
+
+    public void setMenu(String menu) {
+        this.menu = menu;
+    }
+
+    public String getTimeOpen() {
+        return timeOpen;
+    }
+
+    public void setTimeOpen(String timeOpen) {
+        this.timeOpen = timeOpen;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getImages() {
+        return images;
+    }
+
+    public void setImages(String images) {
+        this.images = images;
+    }
+
+    public String getEvaluate() {
+        return evaluate;
+    }
+
+    public void setEvaluate(String evaluate) {
+        this.evaluate = evaluate;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+}
--- ProjectApp/app/src/main/java/app/MainActivity.java
+++ ProjectApp/app/src/main/java/app/MainActivity.java
@@ -22,10 +22,11 @@ public class MainActivity extends AppCompatActivity {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            DatabaseReference firebaseDatabase = FirebaseDatabase.getInstance().getReference("User");
-
-            User user1 = new User("1","Doan","trinhdoanxt@gmail.com","k","abc");
-            firebaseDatabase.setValue(user1);
+           DatabaseReference firebaseDatabase = FirebaseDatabase.getInstance().getReference("Cafe");
+            Cafe cafe1 = new Cafe("Nana", "Khâm Thiên", "Bánh", 20F, "", "",
+                    "", "", "", "");
+//            firebaseDatabase.setValue("Hello,world!");
+            firebaseDatabase.setValue(cafe1);
             return insets;
 
 
