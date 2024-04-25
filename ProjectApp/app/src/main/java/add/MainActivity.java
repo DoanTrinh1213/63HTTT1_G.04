3e3011084a1532bd1fdda07f6cefbf218dd82cad
TrinhThanhDoan <trinhdoan1311xt@gmail.com>
2024-04-25 16:59:12 +0700

    update

---
+++ ProjectApp/app/src/main/java/add/MainActivity.java
@@ -0,0 +1,34 @@
+package add;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import space.app.R;
+import add.User;
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            DatabaseReference firebaseDatabase = FirebaseDatabase.getInstance().getReference("User");
+
+            User user1 = new User("1","Doan","trinhdoanxt@gmail.com","k","abc");
+            firebaseDatabase.setValue(user1);
+            return insets;
+
+
+        });
+    }
+}
