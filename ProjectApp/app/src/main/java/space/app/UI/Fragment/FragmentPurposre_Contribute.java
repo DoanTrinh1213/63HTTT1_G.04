4f0ad4119a82150d343a26e3aefdf72f834f83cc
thanhst <thanhxk2003@gmail.com>
2024-05-14 22:05:58 +0700

    update refix

--- ProjectApp/app/src/main/java/space/app/UI/Fragment/FragmentPurposre_Contribute.java
+++ ProjectApp/app/src/main/java/space/app/UI/Fragment/FragmentPurposre_Contribute.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link InformationApp#newInstance} factory method to
+ * Use the {@link FragmentPurposre_Contribute#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class InformationApp extends Fragment {
+public class FragmentPurposre_Contribute extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class InformationApp extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public InformationApp() {
+    public FragmentPurposre_Contribute() {
         // Required empty public constructor
     }
 
@@ -34,11 +34,11 @@ public class InformationApp extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment InformationApp.
+     * @return A new instance of fragment Purposre_Contribute.
      */
     // TODO: Rename and change types and number of parameters
-    public static InformationApp newInstance(String param1, String param2) {
-        InformationApp fragment = new InformationApp();
+    public static FragmentPurposre_Contribute newInstance(String param1, String param2) {
+        FragmentPurposre_Contribute fragment = new FragmentPurposre_Contribute();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +59,6 @@ public class InformationApp extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_information_app, container, false);
+        return inflater.inflate(R.layout.fragment_purposre__contribute, container, false);
     }
 }
