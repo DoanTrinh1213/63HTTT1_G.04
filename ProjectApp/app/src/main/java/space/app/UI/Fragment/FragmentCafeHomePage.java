d9357a5054c18f69bf4335688bbfccf83e55913e
Yan <phanlannhi777@gmail.com>
2024-05-22 13:33:25 +0700

    update fragment shop

--- ProjectApp/app/src/main/java/space/app/UI/Fragment/FragmentCafeHomePage.java
+++ ProjectApp/app/src/main/java/space/app/UI/Fragment/FragmentCafeHomePage.java
@@ -7,7 +7,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
+import space.app.MainActivity;
 import space.app.R;
 
 /**
@@ -61,6 +64,14 @@ public class FragmentCafeHomePage extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cafe_home_page, container, false);
+        View view = inflater.inflate(R.layout.fragment_cafe_home_page, container, false);
+        LinearLayout linearPurpose = view.findViewById(R.id.linearPurpose);
+        linearPurpose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((MainActivity) getActivity()).replaceFragment(new FragmentPurposre_Contribute(), true);
+            }
+        });
+        return view;
     }
 }
--- ProjectApp/app/src/main/java/space/app/UI/Fragment/FragmentShop.java
+++ ProjectApp/app/src/main/java/space/app/UI/Fragment/FragmentShop.java
@@ -38,7 +38,7 @@ public class FragmentShop extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment shop.
+     * @return A new instance of fragment FragmentShop.
      */
     // TODO: Rename and change types and number of parameters
     public static FragmentShop newInstance(String param1, String param2) {
@@ -62,7 +62,7 @@ public class FragmentShop extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_shop, container, false);
         // edit information
